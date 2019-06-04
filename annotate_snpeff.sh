26eff06c147cad41b51e7eb05a2c478af7fb74bf
Chris Fields <cjfields@illinois.edu>
2019-06-04 18:27:40 -0500

    update annotation

--- annotate_snpeff.sh
+++ annotate_snpeff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -c 4
+#SBATCH -n 2
 #SBATCH --mem 8000
 #SBATCH -A Mayo_Workshop
 #SBATCH -J SnpEff
@@ -8,12 +8,14 @@
 #SBATCH -p classroom
 
 # load the tool environment
-module load GATK/3.7-Java-1.8.0_121
+module load GATK/3.8-1-0-Java-1.8.0_152
+module load snpEff/4.3t-Java-1.8.0_152
 
 # set some shortcuts...
 export BUNDLE_HOME=/home/mirror/gatkbundle
-export SNPEFF=$BUNDLE_HOME/mayo_workshop/snpEff_latest/snpEff/snpEff.jar
-export SNPEFF_CONFIG=$BUNDLE_HOME/mayo_workshop/snpEff_latest/snpEff/snpEff.config
+export REFERENCE=$BUNDLE_HOME/mayo_workshop/2019/human_g1k_v37.fasta
+
+export SNPEFF_CONFIG=$BUNDLE_HOME/mayo_workshop/2019/snpEff.config
 
 # this is our input (VCF)
 export SNP_VCF_FILE=hard_filtered_snps.vcf
@@ -26,12 +28,20 @@ export INDEL_VCF_FILE_OUT=hard_filtered_indels_annotated.vcf
 # to speed this up, we limit the range
 echo -e "20\t15000000\t30000000" > interval.bed
 
-java -Xmx8g -jar $SNPEFF ann GRCh37.75 -geneId -o gatk -v $SNP_VCF_FILE -c $SNPEFF_CONFIG -filterInterval interval.bed > $SNP_VCF_FILE_OUT
+snpEff ann GRCh37.75 \
+    -c $SNPEFF_CONFIG \
+    -geneId -o gatk -v $SNP_VCF_FILE \
+    -filterInterval \
+    interval.bed > $SNP_VCF_FILE_OUT
 
 mkdir snpeff_snp_results
 mv snpEff* snpeff_snp_results
 
-java -Xmx8g -jar $SNPEFF ann GRCh37.75 -geneId -o gatk -v $INDEL_VCF_FILE -c $SNPEFF_CONFIG -filterInterval interval.bed > $INDEL_VCF_FILE_OUT
+snpEff ann GRCh37.75 \
+    -c $SNPEFF_CONFIG \
+    -geneId -o gatk -v $INDEL_VCF_FILE \
+    -filterInterval \
+    interval.bed > $INDEL_VCF_FILE_OUT
 
 mkdir snpeff_indel_results
 mv snpEff* snpeff_indel_results
